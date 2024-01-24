3d686441f72fbcf8b5e2ba8e94ba27286c1a704e
Simple2152 <s00562152@gmail.com>
2024-01-24 15:15:13 +0800

    add

--- server/index.js
+++ server/index.js
@@ -8,14 +8,14 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = 'df8338c09fbf9036cbf86f2bd904da173acca0ba83ddb732722f6ff6034fe870';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
